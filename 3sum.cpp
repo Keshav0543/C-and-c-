90e707ce4c2acfc8f0db3712a6b7b5a64901551a
Keshav0543 <101654133+Keshav0543@users.noreply.github.com>
2025-12-13 09:48:46 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -7,7 +7,6 @@ int main(){
     vector<vector<int>>ans;
     sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-1;i++){
-        if(i>0 && nums[i-1]==nums[i])continue;
         int temp=nums[i]; int left=i+1; int right=nums.size()-1;
         while(left<right){
             if(nums[left]+nums[right]+temp==0){
